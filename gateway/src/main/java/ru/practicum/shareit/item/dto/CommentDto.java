423fdd2fde40e7b4c7a74cbc014018388fbf87c9
Denis <deniska_ledenev@mail.ru>
2026-05-24 15:35:07 +0300

    delete unused imports

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
